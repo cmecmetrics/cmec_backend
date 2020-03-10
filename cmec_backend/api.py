dcb8cca498a09b4e23ce543e27e4fa9d16666175
William Hill <mjh2225@gmail.com>
2020-03-09 17:49:14 -0700

    handle being able to show chart for multiple regions

--- cmec_backend/api.py
+++ cmec_backend/api.py
@@ -19,26 +19,26 @@ ALL_METRICS = {}
 for metric_name in db.session.query(Scalar.metric).order_by(Scalar.metric).distinct():
     ALL_METRICS[metric_name[0]] = {}
 
-print("ALL_METRICS:", ALL_METRICS)
-print("ALL_METRICS type:", type(ALL_METRICS))
+# print("ALL_METRICS:", ALL_METRICS)
+# print("ALL_METRICS type:", type(ALL_METRICS))
 
 ALL_REGIONS = {}
 for region_name in db.session.query(Scalar.region).order_by(Scalar.region).distinct():
     ALL_REGIONS[region_name[0]] = {}
 
-print("ALL_REGIONS:", ALL_REGIONS)
+# print("ALL_REGIONS:", ALL_REGIONS)
 
 ALL_MODELS = {}
 for model_name in db.session.query(Scalar.model).order_by(Scalar.model).distinct():
     ALL_MODELS[model_name[0]] = {}
 
-print("ALL_MODELS:", ALL_MODELS)
+# print("ALL_MODELS:", ALL_MODELS)
 
 ALL_SCALARS = {}
 for scalar_name in db.session.query(Scalar.scalar).order_by(Scalar.scalar).distinct():
     ALL_SCALARS[scalar_name[0]] = {}
 
-print("ALL_SCALARS:", ALL_SCALARS)
+# print("ALL_SCALARS:", ALL_SCALARS)
 
 
 def build_hyperslab_structure(json_object, region, metric, scalar, model):
@@ -67,11 +67,15 @@ def hyperslab():
     print("hyperslab_structure at beginning:", hyperslab_structure)
     # hyperslab_structure.clear()
     data = request.json
-    region, metric, scalar, model = [
+    region_param, metric, scalar, model = [
         data["region"], data["metric"], data["scalar"], data["model"]]
 
-    if region:
-        hyperslab_structure = {region: {}}
+    if region_param:
+        region_param = region_param["value"]
+        print("region_param:", region_param)
+        # region_value = region_param["value"]
+        # print("region_value:", region_value)
+        hyperslab_structure = {region_param: {}}
     else:
         hyperslab_structure = copy.deepcopy(ALL_REGIONS)
 
@@ -79,18 +83,18 @@ def hyperslab():
 
     regions = hyperslab_structure.keys()
     if not metric:
-        for region in regions:
-            hyperslab_structure[region] = copy.deepcopy(ALL_METRICS)
+        for region_name in regions:
+            hyperslab_structure[region_name] = copy.deepcopy(ALL_METRICS)
 
-    print("hyperslab_structure after metric:", hyperslab_structure)
-    print("ALL_METRICS after hyperslab_structure after metric:", ALL_METRICS)
-    print("region:", region)
+    # print("hyperslab_structure after metric:", hyperslab_structure)
+    # print("ALL_METRICS after hyperslab_structure after metric:", ALL_METRICS)
+    print("region_param:", region_param)
     print("metric:", metric)
     print("scalar:", scalar)
     print("model:", model)
     query = db.session.query(Scalar)
-    if region:
-        query = query.filter(Scalar.region == region)
+    if region_param:
+        query = query.filter(Scalar.region == region_param)
     if metric:
         query = query.filter(Scalar.metric.contains(metric))
     if scalar:
@@ -102,6 +106,7 @@ def hyperslab():
     scalar_schema = ScalarSchema(many=True)
     schema_output = scalar_schema.dump(output)
     for result in schema_output:
+        # print("result:", result)
         result_region = result["region"]
         result_metric = result["metric"]
         result_scalar = result["scalar"]
